cca14438b8ccfc1b437223406d1bc089c440cf66
Андрей <andrevyakin@gmail.com>
2022-11-08 20:14:42 +0300

    done task4

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-const userName = prompt("Введите ваш имя").toUpperCase().trim();
-alert(`Вас зовут ${userName}`);
+//task 3
+const userName = prompt("Введите ваш имя.").toUpperCase().trim();
+alert(`Вас зовут ${userName}.`);
+
+//task4
+const userAge = Number(prompt("Сколько Вам лет?").trim());
+alert(`Вам ${userAge} лет.`);
